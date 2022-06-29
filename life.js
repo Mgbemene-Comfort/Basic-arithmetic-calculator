6de9b903a4fb2e67d2008410ac0df08e766bfa9c
Mgbemene Comfort <mgbemenecomfortchika@gmail.com>
2022-06-30 00:40:10 +0100

    repaired and now working calc

--- life.js
+++ life.js
@@ -3,19 +3,21 @@ const operator = prompt('Enter Operator ( either +, -, *, or / ): ');
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
-let result;
+let result = 0;
+if(isNaN(number1) || isNaN(number2)){
+    alert('Wrong input! Refresh the page and provide a number')
+}
 
-if (operator == '+') {
+if(operator == '+'){
     result = number1 + number2;
 }
-if (operator == '-') {
+else if(operator == '-'){
     result = number1 - number2;
 }
-if (operator == '/') {
+else if(operator == '/'){
     result = number1 / number2;
 }
-else {
+else if(operator == '*'){
     result = number1 * number2;
 }
-
-console.log('${number1} ${operator} ${number2} = ${result}');
+document.write(number1 + operator + number2 +' = ' + result);
